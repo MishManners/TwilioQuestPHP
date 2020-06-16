616fe9215fe37869cd2252af87d6e3715ff86709
MishManners <36594527+MishManners@users.noreply.github.com>
2020-06-16 19:44:00 +1000

    Add files via upload
    Still to add bug fixes to third and fourth block of code.

---
+++ bugfixes.php
@@ -0,0 +1,36 @@
+<?php
+
+function isABotCheck($isABot = false)
+{
+  if ($isABot == true) {
+    return true;
+  }
+  return false;
+}
+
+function passwordCheck($providedPassword, $actualPassword)
+{
+  if ($providedPassword != $actualPassword) {
+    return false;
+  }
+  return true;
+}
+
+function securityAnswerCheck($providedAnswer, $actualAnswer)
+{
+  if (strpos($actualAnswer, $providedAnswer) === false) {
+    return false;
+  }
+  return true;
+}
+
+function passwordTriesHitLimit($tries)
+{
+  $limit = 5;
+  if ($tries > $limit && $tries > 0) {
+    return true;
+  }
+  return false;
+}
+
+?>
